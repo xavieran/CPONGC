84648e0d73f7050f212c5c640359b7fab0030a86
Emmanuel Jacyna <xavieran_lives@yahoo.com.au>
2009-12-05 18:24:24 +1100

    AS

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-INCLUDES="-lncurses"
+INCLUDES="-lncurses -lmenu"
 INCLUDE_DIR="./"
-EXE="game"
+EXE="game.x"
 
 gcc -Wall -c *.c $INCLUDES -I$INCLUDE_DIR
 gcc *.o $INCLUDES -I$INCLUDE_DIR -o $EXE

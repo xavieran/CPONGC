5a69d60d33622daeadaae220718d4ae347fc32c3
Rafal Jacyna <rmj@Le-Ville.(none)>
2010-07-01 17:45:57 +1000

    Adding main menu. Ditched animations from fancymenu...

--- src/fancymenu.c
+++ src/fancymenu.c
@@ -1,17 +1,17 @@
 //      fancymenu.c
-//      
+//
 //      Copyright 2009 Emmanuel Jacyna <xavieran.lives@gmail.com>
-//      
+//
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
 //      the Free Software Foundation; either version 2 of the License, or
 //      (at your option) any later version.
-//      
+//
 //      This program is distributed in the hope that it will be useful,
 //      but WITHOUT ANY WARRANTY; without even the implied warranty of
 //      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //      GNU General Public License for more details.
-//      
+//
 //      You should have received a copy of the GNU General Public License
 //      along with this program; if not, write to the Free Software
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -22,16 +22,24 @@
 #include <string.h>
 #include <unistd.h>
 
-//#include "init.h"
+
+enum directions {
+   UP,
+   DOWN,
+   LEFT,
+   RIGHT
+};
 
 
 struct Menu {
     WINDOW* win;
     int selected;
-    int animcnt;
+    int dirty;
     int ITEM_COLOR;
     int SEL_ITEM_COLOR;
     int num_items;
+    int x, y;
+    int width, height;
     char** items;
 };
 
@@ -50,49 +58,72 @@ int longest_str(int str_c, char** str)
 
      return biggest;
 }
-     
-struct Menu* new_menu(int startx, int starty, int num_items, char** items, int ITEM_COLOR, int SEL_ITEM_COLOR)
+
+struct Menu* new_menu(int x, int y, int num_items, char** items, int ITEM_COLOR, int SEL_ITEM_COLOR)
 {
-    
+
     struct Menu* menu = (struct Menu*) malloc(sizeof(struct Menu));
-    
-    int width = longest_str(num_items, items)+4;//the 4 is for the animations
+
+    int width = longest_str(num_items, items)+2;//the 2 is for the '?
     int height = num_items;
-    menu->win = newwin(height, width, starty, startx);
+    menu->win = newwin(height, width, y, x);
     menu->num_items = num_items;
     menu->items = items;
-    menu->animcnt = 0;
     menu->selected = 0;
+    menu->x = x;
+    menu->y = y;
+    menu->width = width;
+    menu->height = height;
     menu->ITEM_COLOR = ITEM_COLOR;
     menu->SEL_ITEM_COLOR = SEL_ITEM_COLOR;
-    
+
     return menu;
 }
 
+//FIX ME!
+/*void erase_rect(WINDOW* win, int y, int x, int length, int height)
+{
+    int i;
+    int j;
+    for (i=y; i<=(y+height); i++){
+        for (j=x; j<(x+length); j++){
+            mvwaddch(win, i, j, ' ');
+        }
+    }
+}*/
 
+void move_selected(struct Menu* menu, int direction)
+{
+    if (direction == DOWN){
+        if (++menu->selected > menu->num_items-1) menu->selected = 0;
+    } else if (direction == UP){
+        if (--menu->selected < 0) menu->selected = menu->num_items-1;
+    }
+    menu->dirty = 1;
+}
 
 void draw_menu(struct Menu* menu)
 {
-    wclear(menu->win);
+
+    if (menu->dirty) {
+        //erase_rect(menu->win, menu->y, menu->x, menu->width, menu->height);
+        menu->dirty = 0;
+        wclear(menu->win);
+    }
+
     int i;
+
     wattron(menu->win, COLOR_PAIR(menu->ITEM_COLOR));
     for (i=0; i<menu->num_items; i++){
-        mvwaddstr(menu->win,i,4, menu->items[i]);
+        mvwaddstr(menu->win,i,2, menu->items[i]);
     }
     wattroff(menu->win, COLOR_PAIR(menu->ITEM_COLOR));
-    
-    wattron(menu->win, COLOR_PAIR(menu->SEL_ITEM_COLOR));
-    mvwaddstr(menu->win, menu->selected, 4, menu->items[menu->selected]);
-    wattroff(menu->win, COLOR_PAIR(menu->SEL_ITEM_COLOR));
-    
-    wattron(menu->win, COLOR_PAIR(menu->ITEM_COLOR));
-    mvwaddstr(menu->win, menu->selected, 0, ">>>");
-    wattroff(menu->win, COLOR_PAIR(menu->ITEM_COLOR));
 
-    mvwaddch(menu->win, menu->selected, menu->animcnt, '>' | COLOR_PAIR(menu->SEL_ITEM_COLOR));
-    if (++menu->animcnt > 2) menu->animcnt = 0;
-    refresh(); //TODO: look into this refresh issues...
-    wrefresh(menu->win);
+    wattron(menu->win, COLOR_PAIR(menu->SEL_ITEM_COLOR));
+    mvwaddstr(menu->win, menu->selected, 2, menu->items[menu->selected]);
+    mvwaddch(menu->win, menu->selected, 0, '>');
+    wattroff(menu->win, COLOR_PAIR(menu->SEL_ITEM_COLOR));
+
 }
 
 int poll_menu(struct Menu* menu)
@@ -104,16 +135,17 @@ int poll_menu(struct Menu* menu)
         draw_menu(menu);
         switch (ch){
             case KEY_UP:
-                if (--menu->selected < 0) menu->selected = menu->num_items-1;
+                move_selected(menu, UP);
                 break;
             case KEY_DOWN:
-                if (++menu->selected > menu->num_items-1) menu->selected = 0;
+                move_selected(menu, DOWN);
                 break;
             case KEY_RIGHT:
                 return menu->selected;
                 break;
             }
-
+        refresh(); //TODO: look into this refresh issues...
+        wrefresh(menu->win);
         usleep(125*1000);
     }
     return 0;
@@ -133,7 +165,7 @@ int poll_menu(struct Menu* menu)
     init_pair(BLUE_ON_BLACK, COLOR_BLUE, COLOR_BLACK);
     init_pair(GREEN_ON_BLACK, COLOR_GREEN, COLOR_BLACK);
     nodelay(stdscr, 1);
-    
+
     struct Menu* menu = new_menu(10, 3, argc, argv, BLUE_ON_BLACK, GREEN_ON_BLACK);
     int x;
     x = poll_menu(menu);
--- src/pong.c
+++ src/pong.c
@@ -1,19 +1,19 @@
 
 
 //      pong.c
-//      
+//
 //      Copyright 2009 Emmanuel Jacyna <xavieran.lives@gmail.com>
-//      
+//
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
 //      the Free Software Foundation; either version 2 of the License, or
 //      (at your option) any later version.
-//      
+//
 //      This program is distributed in the hope that it will be useful,
 //      but WITHOUT ANY WARRANTY; without even the implied warranty of
 //      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //      GNU General Public License for more details.
-//      
+//
 //      You should have received a copy of the GNU General Public License
 //      along with this program; if not, write to the Free Software
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -44,6 +44,8 @@
 //Initial ball velocity
 #define EASY_BALL_VX .2
 #define EASY_BALL_VY .075
+#define HARD_BALL_VX .5
+#define HARD_BALL_VY .4
 
 //Amount ball velocity increases per hit
 #define EASY_BALL_V_INC .05
@@ -51,6 +53,11 @@
 #define EASY_AI_WAIT 20
 
 #define INFO_WIN_HEIGHT 3
+#define LOWEST_XRES 40
+#define LOWEST_YRES 12
+
+#define SOUND_ON 0
+#define SOUND_OFF 1
 
 enum difficulties {
     EASY,
@@ -97,7 +104,7 @@ int randint(int h)
     double r = rand();
     return (int) (r / (((double) RAND_MAX + (double) 1) / (double) h));
 }
-    
+
 
 int sign(float x)
 {
@@ -117,7 +124,7 @@ int my_int(float x)
     if ((x - ((int) x)) > .5) return ((int) x) + 1;
     return (int) x;
 }
-    
+
 /*++++++++++++++
  BALL STRUCTURE
  ++++++++++++++*/
@@ -142,7 +149,7 @@ void destroy_ball(struct Ball* ball)
 {
     free(ball);
     ball = NULL;
-}                  
+}
 
 char* str_ball(struct Ball* ball)
 {
@@ -162,7 +169,7 @@ struct Ball move_ball_f(struct Ball ball)
 }
 
 void move_ball(struct Ball* ball)
-{   
+{
     *ball = move_ball_f(*ball);
 }
 
@@ -189,14 +196,14 @@ void move_ball_xy(struct Ball* ball, int x, int y)
 struct Paddle {
 /* The paddle looks like this:
  * 0,0
- * | x,y 
+ * | x,y
  * |
  * |
  * |
  * | x,y+width (width=5)
  * 0,y
- * 
- */   
+ *
+ */
     int x; int y; //x,y coordinates
     int px; int py; //the previous position of paddle
     int width; //the width
@@ -215,7 +222,7 @@ struct Paddle* make_paddle(int x, int y, int width, int vel)
 }
 
 void destroy_paddle(struct Paddle* paddle)
-{             
+{
     free(paddle);
     paddle = NULL;
 }
@@ -231,7 +238,7 @@ char* str_paddle(struct Paddle* paddle)
 
 struct Paddle move_paddle_dir_f(struct Paddle paddle, int dir)
 //Move paddle in the specified direction
-{   
+{
     switch (dir){
         case UP:
             paddle.py = paddle.y;
@@ -277,6 +284,7 @@ struct Game {
     int width;
     int height;
     int difficulty;
+    int sound;
     int p1_score;
     int p2_score;
     char* p1_name;
@@ -284,7 +292,7 @@ struct Game {
 };
 
 
-struct Game* make_game(int max_width, int max_height, int difficulty, char* p1_name, char* p2_name)
+struct Game* make_game(int max_width, int max_height, int difficulty, int sound, char* p1_name, char* p2_name)
 {
     struct Game* p = malloc(sizeof(struct Game));
     if (p == NULL) die_no_memory();
@@ -293,6 +301,7 @@ struct Game* make_game(int max_width, int max_height, int difficulty, char* p1_n
     p->width = max_width;
     p->height = max_height - INFO_WIN_HEIGHT - 1;
     p->difficulty = difficulty;
+    p->sound = sound;
     p->p1_score = 0;
     p->p2_score = 0;
     p->p1_name = p1_name;
@@ -301,7 +310,7 @@ struct Game* make_game(int max_width, int max_height, int difficulty, char* p1_n
 }
 
 void destroy_game(struct Game* game)
-{             
+{
     free(game);
     game = NULL;
 }
@@ -323,9 +332,9 @@ char* str_game(struct Game* game)
 
 
 //Shamelessly stolen from: http://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect/565282#565282
-// Returns 1 if the lines intersect, otherwise 0. In addition, if the lines 
+// Returns 1 if the lines intersect, otherwise 0. In addition, if the lines
 // intersect the intersection point may be stored in the floats i_x and i_y.
-int lines_intersect(float p0_x, float p0_y, float p1_x, float p1_y, 
+int lines_intersect(float p0_x, float p0_y, float p1_x, float p1_y,
                     float p2_x, float p2_y, float p3_x, float p3_y)
 {
     float s1_x, s1_y, s2_x, s2_y;
@@ -346,7 +355,7 @@ int lines_intersect(float p0_x, float p0_y, float p1_x, float p1_y,
 
 int ball_intersect_paddle(struct Ball* ball, struct Paddle* paddle)
 {
-    if (lines_intersect(ball->x, ball->y, ball->px, ball->py, paddle->x, 
+    if (lines_intersect(ball->x, ball->y, ball->px, ball->py, paddle->x,
                         paddle->y + paddle->width, paddle->x, paddle->y)){
         return 1;
     }
@@ -378,7 +387,7 @@ float collision_dist_prcnt(struct Ball* ball, struct Paddle* paddle)
 }
 
 
-// Drawing Functions: 
+// Drawing Functions:
 void initialize_colors()
 {
     //We want color
@@ -393,7 +402,7 @@ void initialize_colors()
     init_pair(BLUE_ON_BLACK, COLOR_BLUE, COLOR_BLACK);
     init_pair(RED_ON_BLACK, COLOR_RED, COLOR_BLACK);
     init_pair(YELLOW_ON_BLACK, COLOR_YELLOW, COLOR_BLACK);
-    init_pair(GREEN_ON_BLACK, COLOR_GREEN, COLOR_BLACK);  
+    init_pair(GREEN_ON_BLACK, COLOR_GREEN, COLOR_BLACK);
 }
 
 int die()
@@ -434,7 +443,7 @@ void erase_draw_ball(WINDOW* win, struct Ball* ball, int color)
 }
 
 void erase_paddle(WINDOW* win, struct Paddle* paddle)
-{   
+{
     erase_rect(win, paddle->py, paddle->px, 1, paddle->width);
 }
 
@@ -455,34 +464,46 @@ void erase_draw_paddle(WINDOW* win, struct Paddle* paddle, int color)
     erase_paddle(win, paddle);
     //Now draw it again
     draw_paddle(win, paddle, color);
-}                     
+}
 
-//For now, we will just use random values for the paddle widths and lengths...
+void draw_strings(WINDOW* win, int y, int x, char** str_arr, int arr_c)
+{
+    int i;
+    for (i=0; i < arr_c; i++){
+        mvwaddstr(win, i + y, x, str_arr[i]);
+    }
+}
+
 int play_game(struct Game* game, int ai)
 {
     WINDOW* win = newwin(game->height, game->width, INFO_WIN_HEIGHT, 0);
     WINDOW* info_win = newwin(INFO_WIN_HEIGHT, game->max_width, 0, 0);
-    
-    
+
+
     Timer* timer = sgl_timer_new();
-    
+
     struct Ball* ball = make_ball(game->width/2, game->height/2, EASY_BALL_VX, EASY_BALL_VY);
     struct Paddle* paddle1 = make_paddle(0, game->height/2, 5, 3);
     struct Paddle* paddle2 = make_paddle(game->width-1, game->height/2, 5, 3);
-    
+
     //Used for checking where paddles are going to be...
     struct Paddle check_pad;
-    
+
     int ai_wait = 0;
-    
+
     int key;
-    
+
     char* tmp_str = malloc(sizeof(char) * MAX_STRING_LENGTH);
 
-    
+
     while ((key = getch())){
 
         switch (key) {
+            case 'm':
+                if (game->sound) game->sound = 0;
+                else game->sound = 1;
+                break;
+
             case 'w':
                 check_pad = move_paddle_dir_f(*paddle1, UP);
                 if (check_pad.y + paddle1->vel < 0){
@@ -490,7 +511,7 @@ int play_game(struct Game* game, int ai)
                 }else { move_paddle_dir(paddle1, UP);
                 }
                 break;
-                
+
             case 's':
                 check_pad = move_paddle_dir_f(*paddle1, UP);
                 if ((paddle1->y + paddle1->vel + paddle1->width)  > game->height){
@@ -498,7 +519,7 @@ int play_game(struct Game* game, int ai)
                 }else { move_paddle_dir(paddle1, DOWN);
                 }
                 break;
-                
+
             case KEY_UP:
                 check_pad = move_paddle_dir_f(*paddle2, UP);
                 if ((check_pad.y + paddle2->vel) < 0){
@@ -506,7 +527,7 @@ int play_game(struct Game* game, int ai)
                 }else { move_paddle_dir(paddle2, UP);
                 }
                 break;
-                
+
             case KEY_DOWN:
                 check_pad = move_paddle_dir_f(*paddle2, DOWN);
                 if (((check_pad.y + check_pad.width) - paddle2->vel)  > game->height){
@@ -530,92 +551,158 @@ int play_game(struct Game* game, int ai)
             }
             ai_wait = 0;
         }
-        
+
         //Ball has collided with paddle1
         if (sgl_timer_elapsed_milliseconds(timer) > 10){
             sgl_timer_reset(timer);
             ai_wait++;
-            
+
             if ((ball->x <= paddle1->x) && ball_intersect_paddle(ball, paddle1)){
-                fbeep(660, 10);
+                if (game->sound) fbeep(660, 10);
                 //Move the ball 1 forward from paddle, this paddle is on left
                 move_ball_xy(ball, paddle1->x + 1, ball->y);
                 //Flip the ball's direction
                 ball->vx = -ball->vx;
-                
+
                 float prcnt = collision_dist_prcnt(ball, paddle1);
                 float vxi = (1 - prcnt) * EASY_BALL_V_INC;
                 float vyi = prcnt * EASY_BALL_V_INC;
-                
+
                 ball->vx = sign(ball->vx) * (fabsf(ball->vx) + vxi);
                 ball->vy = sign(ball->vy) * (fabsf(ball->vy) + vyi);
-    
+
             } else if (ball->x <= paddle1->x){
-                fbeep(550, 20);
+                if (game->sound) fbeep(550, 20);
                 //Increase score...
                 game->p2_score++;
                 ball->vx = EASY_BALL_VX;
                 ball->vy = EASY_BALL_VY;
                 move_ball_xy(ball, game->width / 2, game->height / 2);
             }
-            
+
             //Ball has collided with paddle2
             if ((ball->x >= paddle2->x) && ball_intersect_paddle(ball, paddle2)){
                 fbeep(660, 10);
                 //Move the ball 1 back from the paddle, this paddle is the one on right...
                 move_ball_xy(ball, paddle2->x - 1, ball->y);
                 ball->vx = -ball->vx;
-                
+
                 float prcnt = collision_dist_prcnt(ball, paddle2);
                 float vxi = (1 - prcnt) * EASY_BALL_V_INC;
                 float vyi = prcnt * EASY_BALL_V_INC;
-                
+
                 ball->vx = sign(ball->vx) * (fabsf(ball->vx) + vxi);
                 ball->vy = sign(ball->vy) * (fabsf(ball->vy) + vyi);
-    
+
             } else if (ball->x >= paddle2->x){
-                //make bad noise :(
-                fbeep(550, 20);
-                //Increase score...
+                if (game->sound) fbeep(550, 20);
                 game->p1_score++;
                 ball->vx = -(EASY_BALL_VX);
                 ball->vy = EASY_BALL_VY;
                 move_ball_xy(ball, game->width / 2, game->height / 2);
             }
-            
+
             //We need to erase it now because???
             erase_ball(win, ball);
-    
+
             if ((ball->y > game->height) || (ball->y < 0)) ball->vy = -ball->vy;
             move_ball(ball);
         }
-        
-        
+
+
         erase_draw_ball(win, ball, RED);
         erase_draw_paddle(win, paddle1, BLUE);
         erase_draw_paddle(win, paddle2, GREEN);
-        
+
         //Display some info
         box(info_win, 0 , 0);
         snprintf(tmp_str, MAX_STRING_LENGTH, "%s: %d", game->p1_name, game->p1_score);
         mvwaddstr(info_win, 1, 1, tmp_str);
         snprintf(tmp_str, MAX_STRING_LENGTH, "%s: %d", game->p2_name, game->p2_score);
         mvwaddstr(info_win, 1, game->width - strlen(tmp_str) - 1, tmp_str);
-        
+
         wrefresh(info_win);
         wrefresh(win);
         refresh();
     }
-    
+
     return 0;
 }
-    
-int main(int argc, char** argv)
-{
-    //Eliminate compiler warning :)
-    (void) argc;
-    (void) argv;
 
+struct Game* change_resolution(WINDOW* win, struct Game* game, int max_xres, int max_yres)
+{
+    int max_x, max_y;
+    getmaxyx(win, max_y, max_x);
+
+    max_yres -= INFO_WIN_HEIGHT;
+
+    char* width_str = malloc(sizeof(char) * MAX_STRING_LENGTH);
+    char* height_str = malloc(sizeof(char) * MAX_STRING_LENGTH);
+
+    int selected = 0;
+
+    int c;
+    while ((c = getch()) != 'q')
+    {
+        switch (c){
+            case KEY_RIGHT:
+                if (selected){
+                    game->height += 5;
+                    if (game->height > max_yres) game->height = max_yres;
+                } else {
+                    game->width += 5;
+                    if (game->width > max_xres) game->width = max_xres;
+                }
+                break;
+            case KEY_LEFT:
+                if (selected){
+                    game->height -= 5;
+                    if (game->height < LOWEST_YRES) game->height = LOWEST_YRES;
+                } else {
+                    game->width -= 5;
+                    if (game->width < LOWEST_XRES) game->width = LOWEST_XRES;
+                }
+                break;
+            case KEY_UP:
+                if (selected) selected = 0;
+                else selected = 1;
+                break;
+            case KEY_DOWN:
+                if (selected) selected = 0;
+                else selected = 1;
+                break;
+            }
+
+        snprintf(width_str, MAX_STRING_LENGTH, "Width: < %d >", game->width);
+        snprintf(height_str, MAX_STRING_LENGTH, "Height: < %d >", game->height);
+
+        wattron(win, COLOR_PAIR(GREEN_ON_BLACK));
+        if (selected) mvwaddstr(win, max_y / 4 + 1, max_x / 4, height_str);
+        else mvwaddstr(win, max_y / 4, max_x / 4, width_str);
+        wattroff(win, COLOR_PAIR(GREEN_ON_BLACK));
+
+        if (selected) mvwaddstr(win, max_y / 4, max_x / 4, width_str);
+        else mvwaddstr(win, max_y / 4 + 1, max_x / 4, height_str);
+
+        erase_rect(win, max_y / 4, max_x, strlen(width_str), 2);
+    }
+
+    game->max_height = game->height;
+    game->max_width = game->width;
+    return game;
+}
+
+
+char* game_title[6] = {"    ____                    ",\
+                       "   / __ \\____  ____  ____ _ ",\
+                       "  / /_/ / __ \\/ __ \\/ __ `/ ",\
+                       " / ____/ /_/ / / / / /_/ /  ",\
+                       "/_/    \\____/_/ /_/\\__, /   ",\
+                       "                  /____/    "};
+
+
+int main_menu(void)
+{
     WINDOW* screen = initscr();
     initialize_colors();
     cbreak();
@@ -624,28 +711,95 @@ int main(int argc, char** argv)
     curs_set(0); //turn cursor off
     nodelay(stdscr, 1);
 
-    
-    char* choices[2] = {"AI", "No AI"};
-    struct Menu* main_menu = new_menu(20, 4, 2, choices, GREEN_ON_BLACK, YELLOW_ON_BLACK);
-    struct Game* game;
-    int ai;
-
     int max_x, max_y;
     getmaxyx(screen, max_y, max_x);
-    
-    switch (poll_menu(main_menu)){
-        case 0:
-            game = make_game(max_x, max_y, EASY, "Computer", "Human");
-            ai = 1;
-            break;
-        case 1:
-            game = make_game(max_x, max_y, EASY, "Gerald", "Joey");
-            ai = 0;
-            break;
+
+    int key;
+    int difficulty = EASY;
+
+    char* title_menu_l[4] = {"Play!", "Options", "Help", "Quit"};
+    struct Menu* title_menu = new_menu(max_x / 8, 8, 4, title_menu_l, BLUE_ON_BLACK, GREEN_ON_BLACK);
+
+    char* opts_menu_l[3] = {"Difficulty", "Sound?", "Change Resolution"};
+    struct Menu* opts_menu = new_menu(max_x / 8, 8, 3, opts_menu_l, BLUE_ON_BLACK, GREEN_ON_BLACK);
+
+    struct Ball* ball = make_ball(max_x / 2, max_y / 4, HARD_BALL_VX, HARD_BALL_VY);
+    struct Paddle* paddle = make_paddle(0, max_y / 2, 5, 2);
+
+    //REMOVE THIS
+    struct Game* game = make_game(max_x, max_y, EASY, SOUND_ON, "Computer", "Human");
+
+    Timer* timer = sgl_timer_new();
+
+    while (key != 'q'){
+        switch (key = getch()){
+            case KEY_DOWN:
+                move_selected(title_menu, DOWN);
+                break;
+            case KEY_UP:
+                move_selected(title_menu, UP);
+                break;
+            case KEY_RIGHT:
+                switch (title_menu->selected){
+                    case 0: //Play game
+                        play_game(game, 1);
+                        break;
+                    case 1: //Options
+                        break;
+                    case 2: //Help
+                        break;
+                    case 3: //Quit
+                        return 0;
+                        break;
+                    }
+                break;
+        }
+
+        //Move and bound ball
+        if (sgl_timer_elapsed_milliseconds(timer) > 20){
+            sgl_timer_reset(timer);
+            move_ball(ball);
+            if ((ball->y > max_y) || (ball->y < 0)) ball->vy = -ball->vy;
+            if ((ball->x > max_x) || (ball->x < 0)) ball->vx = -ball->vx;
+            erase_draw_ball(screen, ball, RED);
+
+            //Move paddle
+            switch (ball_in_paddle(ball, paddle)) {
+                case 0:
+                    break;
+                case 1:
+                    if (ball->vx < 0) move_paddle_dir(paddle , UP);
+                    break;
+                case -1:
+                    if (ball->vx < 0) move_paddle_dir(paddle, DOWN);
+                    break;
+            }
+
+            erase_draw_paddle(screen, paddle, BLUE);
+        }
+
+
+
+
+        draw_strings(screen, 1, max_x / 2 - strlen(game_title[0]), game_title, 6);
+        draw_menu(title_menu);
+        wrefresh(title_menu->win);
     }
-        
-    play_game(game, ai);
-    
+
+
+    //
+    //game = change_resolution(screen, game, max_x, max_y);
+
+    //play_game(game, 1);
+}
+
+int main(int argc, char** argv)
+{
+    //Eliminate compiler warning :)
+    (void) argc;
+    (void) argv;
+    fbeep(1000, 10);
+    main_menu();
     die();
     return 0;
 }
